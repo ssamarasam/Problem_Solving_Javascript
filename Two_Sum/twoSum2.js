5233ec9d2a03824eca7b7dc0037f1266f3f13bb2
ssamarasam <samcsegct@gmail.com>
2023-04-20 13:36:57 +0530

    added twosum solution

---
+++ Two_Sum/twoSum2.js
@@ -0,0 +1,20 @@
+function twoSum(array, sum) {
+    let left = 0;
+    let right = array.length -1;
+    while(left < right) {
+        let total = array[left] + array[right];
+        if (total === sum) {
+            return [array[left], array[right]]
+        } else if ( total > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return 'Not found';
+}
+
+let arr = [-3, -2, 0, 1, 2, 4]
+
+let result = twoSum(arr, 0);
+console.log(result);
